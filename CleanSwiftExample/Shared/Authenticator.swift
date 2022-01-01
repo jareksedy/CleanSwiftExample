f7a2c03f105a6aa6c7f09edc40db2f8e762061a8
Jarek Šedý <jareksedy@icloud.com>
2022-01-01 19:57:20 +0600

    Added Authenticator worker.

---
+++ CleanSwiftExample/Shared/Authenticator.swift
@@ -0,0 +1,16 @@
+//
+//  Authenticator.swift
+//  CleanSwiftExample
+//
+//  Created by Ярослав on 01.01.2022.
+//
+
+import Foundation
+
+class Authenticator {
+    private let passcodes = ["1234", "4359", "8888", "9761"]
+    
+    func authenticate(with passcode: String) -> Bool {
+        return passcodes.contains(passcode)
+    }
+}
